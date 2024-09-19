0ee41a369b2b9a59f37fc051986643baaec8ba67
microwesttechnologies <microwesttechnologies@gmail.com>
2024-09-19 00:12:14 -0500

    Dashboard Component

--- UI/modules/dashboard/dashboard.component.ts
+++ UI/modules/dashboard/dashboard.component.ts
@@ -1,19 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { CardModule } from 'primeng/card';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { CommonModule } from '@angular/common';
+import { Table } from 'primeng/table';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TagModule } from 'primeng/tag';
+import { CustomerService } from 'UI/services/costumer.service';
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AvatarGroupModule, AvatarModule, CardModule, CommonModule, TableModule],
+  imports: [AvatarGroupModule, AvatarModule, CardModule, CommonModule, TableModule, ButtonModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
-})
-export class DashboardComponent {
+  styleUrl: './dashboard.component.scss',
+  providers: [CustomerService]
 
+})
+export class DashboardComponent implements OnInit {
+
+  customers!: any[];
+
+  representatives!: any[];
+
+  value!: any;
+
+  
+
+  statuses!: any[];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
+
+  searchValue: string | undefined;
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+      this.customerService.getCustomersLarge().then((customers) => {
+          this.customers = customers;
+          this.loading = false;
+
+          this.customers.forEach((customer) => (customer.date = new Date(<Date>customer.date)));
+      });
+
+      this.representatives = [
+          { name: 'Amy Elsner', image: 'amyelsner.png' },
+          { name: 'Anna Fali', image: 'annafali.png' },
+          { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+          { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+          { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+          { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+          { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+          { name: 'Onyama Limba', image: 'onyamalimba.png' },
+          { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+          { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
+      ];
+
+      this.statuses = [
+          { label: 'Unqualified', value: 'unqualified' },
+          { label: 'Qualified', value: 'qualified' },
+          { label: 'New', value: 'new' },
+          { label: 'Negotiation', value: 'negotiation' },
+          { label: 'Renewal', value: 'renewal' },
+          { label: 'Proposal', value: 'proposal' }
+      ];
+  }
+
+  clear(table: Table) {
+      table.clear();
+      this.searchValue = ''
+  }
+
+  getSeverity(status: string) {
+    switch (status.toLowerCase()) {
+        case 'unqualified':
+            return 'danger';
+        case 'qualified':
+            return 'success';
+        case 'new':
+            return 'info';
+        case 'negotiation':
+            return 'warning';
+        case 'renewal':
+            return null;
+        default:
+            return ''; 
+    }
+}
+
+  
   items: any[] = [
     {
       label:"Total Historias clínicas",
