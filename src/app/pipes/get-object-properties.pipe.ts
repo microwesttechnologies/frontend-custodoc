d2385a252251bafbd6490865696b1978725c848f
jdmunozPirani <david.gallego@pirani.co>
2025-02-18 19:46:51 -0500

    Deploy

--- src/app/pipes/get-object-properties.pipe.ts
+++ src/app/pipes/get-object-properties.pipe.ts
@@ -2,7 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'getObjectProperties', standalone: true })
 export class GetObjectPropertiesPipe implements PipeTransform {
-  transform(object: object): string[] {
-    return typeof object === 'object' ? Object.keys(object) : [];
+  transform(
+    object: object,
+    property: 'keys' | 'values' | 'entries' = 'keys'
+  ): string[] {
+    return typeof object === 'object' ? Object[property](object) : [];
   }
 }
